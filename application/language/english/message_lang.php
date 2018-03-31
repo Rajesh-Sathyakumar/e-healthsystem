769f604203d6edbcaba777597dd09c352c85db56
Swarna Swapna <swarna.ilas@gmail.com>
2018-03-31 08:14:07 +0530

    login with google and lang switcher

---
+++ application/language/english/message_lang.php
@@ -0,0 +1,45 @@
+<?php
+/*english language*/
+$lang['hospitalName'] = 'hospitalName';
+$lang['hospitalshortName']='hospitalshortName';
+$lang['hospitaltype']='hospitaltype';
+$lang['Pincode']='Pincode';
+$lang['HospitalInchargeName']='HospitalInchargeName';
+$lang['HospitalInchargemobile']='HospitalInchargemobile';
+$lang['HospitalInchargePhone']='HospitalInchargePhone';
+$lang['HospitalInchargeEmail']='Hospital Incharge Email';
+$lang['ownerName']='ownerName';
+$lang['GeneralBeds']='GeneralBeds';
+$lang['DayCareBeds']='DayCareBeds';
+$lang['ICUBeds']='ICUBeds';
+$lang['ICCUBeds']='ICCUBeds';
+$lang['HDUBeds']='HDUBeds';
+$lang['Majorots']='Majorots';
+$lang['MinorOts']='MinorOts';
+$lang['HospitalAddress']='HospitalAddress';
+$lang['Latitude']='Latitude';
+$lang['Longitude']='Longitude';
+$lang['panNumber']='panNumber';
+$lang['clinicalRegistrationNumber']='clinicalRegistrationNumber';
+$lang['panCardHolderName']='panCardHolderName';
+$lang['erviceTaxRegistrationNumber']='erviceTaxRegistrationNumber';
+$lang['bankName']='bankName';
+$lang['bankAccountNumber']='bankAccountNumber';
+$lang['IFSCCode']='IFSCCode';
+$lang['branchAddress']='branchAddress';
+$lang['payeeName']='payeeName';
+$lang['numberofFullTimePhysicians']='numberofFullTimePhysicians';
+$lang['remarks']='remarks';
+$lang['fullTimeConsultants']='fullTimeConsultants';
+$lang['PartTimeConsultants']='PartTimeConsultants';
+$lang['VisitingConsultants']='VisitingConsultants';
+$lang['dutyDoctors']='dutyDoctors';
+$lang['generalNurses']='generalNurses';
+$lang['pharmacytype']='pharmacytype';
+$lang['state']='state';
+$lang['District']='District';
+$lang['location']='location';
+$lang['nabh']='nabh';
+$lang['comments']='comments';
+
+
